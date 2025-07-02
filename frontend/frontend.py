d2c7c79741eae92f940b6d8c466271a609e1ce37
R <rinakapoor@Rinas-MacBook-Air.local>
2025-07-02 13:24:26 -0700

    test3

--- frontend/frontend.py
+++ frontend/frontend.py
@@ -1,7 +1,7 @@
 print('Hello world')
 
 print('This is the frontend module')
-
+#third test
 def frontend_function():
     print('This is a function in the frontend module')
     return 'frontend_function result'
