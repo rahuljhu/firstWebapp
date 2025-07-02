b7f3bdf2e5857340ddf67ba3ba36247be9a22ee0
Rina Kapoor <rinakapoor@Rinas-MacBook-Air.local>
2025-07-02 13:17:01 -0700

    second commit

--- frontend/frontend.py
+++ frontend/frontend.py
@@ -1,3 +1,7 @@
 print('Hello world')
 
 print('This is the frontend module')
+
+def frontend_function():
+    print('This is a function in the frontend module')
+    return 'frontend_function result'
